be77914c653ad1b2afca38cc75b1e86cf786263c
Andrii Logan Zvorygin <andrii@liberit.ca>
2022-03-09 16:35:41 -0500

    updated mp3s to have copula

--- pyacqrisdyim.js
+++ pyacqrisdyim.js
@@ -8,6 +8,8 @@ fs.readFile(file, "utf8", function(err, pyashWords) {
   const kwickwonlwat = pyashWords.toString().split("\n");;
 	let kle7nmyin = ""
 	console.log(JSON.stringify(kwickwonlwat));
+	  let myin = `espeak-ng -x -s300 -w ja.wav -vpy+m2 ja};` 
+  	  console.log(clattsen.execSync(myin).toString());
   kwickwonlwat.map((kwichlas, htik)  => {
 	  if (kwichlas.length == 0) return 0;
 	  kwichlas = kwichlas.split(/,(.+)/);
@@ -16,10 +18,9 @@ fs.readFile(file, "utf8", function(err, pyashWords) {
           myin = `espeak-ng -w ${htik}-1.wav -s450 -x -ven ${kwichlas[1]};`
   	  console.log(clattsen.execSync(myin).toString());
 	  //console.log(myin);
-	  kle7nmyin += ` ${htik}-0.wav ${htik}-1.wav `
+	  kle7nmyin += ` ${htik}-0.wav ja.wav ${htik}-1.wav `
   });
   let grutkle7nmyin = kle7nmyin.split(" ").reverse().join(" ");
-  console.log(grutkle7nmyin);
   clattsen.execSync("sox " + kle7nmyin + grutkle7nmyin + ` ${file}.mp3`);
   clattsen.execSync("rm *.wav");
   
