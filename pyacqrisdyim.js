e1b7b40093d9d4b65c2ca21e34f644fe9bb4cea9
Andrii Logan Zvorygin <andrii@liberit.ca>
2022-03-09 16:46:48 -0500

    updated a different copula version

--- pyacqrisdyim.js
+++ pyacqrisdyim.js
@@ -7,8 +7,11 @@ const file = process.argv[2];
 fs.readFile(file, "utf8", function(err, pyashWords) { 
   const kwickwonlwat = pyashWords.toString().split("\n");;
 	let kle7nmyin = ""
+	let grutkle7nmyin = ""
 	console.log(JSON.stringify(kwickwonlwat));
-	  let myin = `espeak-ng -x -s300 -w ja.wav -vpy+m2 ja};` 
+	  let myin = `espeak-ng -x -s450 -w na.wav -vpy+f2 na};` 
+  	  console.log(clattsen.execSync(myin).toString());
+	  myin = `espeak-ng -x -s450 -w kali.wav -vpy+f2 kali};` 
   	  console.log(clattsen.execSync(myin).toString());
   kwickwonlwat.map((kwichlas, htik)  => {
 	  if (kwichlas.length == 0) return 0;
@@ -18,9 +21,9 @@ fs.readFile(file, "utf8", function(err, pyashWords) {
           myin = `espeak-ng -w ${htik}-1.wav -s450 -x -ven ${kwichlas[1]};`
   	  console.log(clattsen.execSync(myin).toString());
 	  //console.log(myin);
-	  kle7nmyin += ` ${htik}-0.wav ja.wav ${htik}-1.wav `
+	  kle7nmyin += ` ${htik}-0.wav na.wav ${htik}-1.wav kali.wav`
+          grutkle7nmyin = ` ${htik}-1.wav na.wav ${htik}-0.wav kali.wav`
   });
-  let grutkle7nmyin = kle7nmyin.split(" ").reverse().join(" ");
   clattsen.execSync("sox " + kle7nmyin + grutkle7nmyin + ` ${file}.mp3`);
   clattsen.execSync("rm *.wav");
   
