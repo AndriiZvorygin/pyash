afbff67f9a70b46e204ae58d8cbba89bb1a8dec7
Andrii Logan Zvorygin <andrii@liberit.ca>
2025-12-13 05:23:46 -0500

    add translation roundtrip test and signature coverage

--- program/verbs/exchange/translation.mjs
+++ program/verbs/exchange/translation.mjs
@@ -282,6 +282,10 @@ export const signatures = [
     handler: translation_from_text_to_name_text
   },
   {
+    signatureWords: ["be", "translation", "become", "name", "num", "fromstate", "name", "num", "obj", "name", "text", "to", "name", "num"],
+    handler: translation_from_text_to_name_text
+  },
+  {
     signatureWords: ["be", "translation", "become", "name", "num", "from", "text", "fromstate", "name", "num", "to", "name", "text"],
     handler: translation_from_text_to_name_text
   },
--- quiz/translation.test.mjs
+++ quiz/translation.test.mjs
@@ -43,3 +43,34 @@ test("translation from Pyash conditional to English", async () => {
   assert.ok(translation);
   assert.match(translation, /if 3 is tiny from 5 then do add 1 to total/);
 });
+
+test("translation Pyash -> English -> Pyash roundtrip", async () => {
+  forget();
+
+  const pyashProgram = [
+    "subj name alpha obj num 1 be number ya",
+    "obj num 2 to name alpha be add do"
+  ].join("\\n");
+
+  // Pyash -> English
+  const toEnglish = parse(
+    `from text quoted.pyash.${pyashProgram}.pyash.quoted from state pyash to state english to name english_out be translation do`
+  );
+  const englishResult = await interpret(toEnglish);
+  const englishText = englishResult?.obj?.text ?? englishResult?.value?.text;
+  assert.ok(englishText, "should produce english text");
+
+  // English -> Pyash
+  const backToPyash = parse(
+    `from text quoted.pyash.${englishText}.pyash.quoted from state english to state pyash to name pyash_out be translation do`
+  );
+  const pyashResult = await interpret(backToPyash);
+  const sentences = pyashResult?.obj?.sentences ?? pyashResult?.value?.sentences;
+  assert.ok(Array.isArray(sentences), "roundtrip should yield sentences array");
+  assert.equal(sentences.length, 2);
+  assert.equal(sentences[0].subj?.name, "alpha");
+  assert.equal(sentences[0].obj?.num, 1);
+  assert.equal(sentences[1].be, "add");
+  assert.equal(sentences[1].obj?.num, 2);
+  assert.equal(sentences[1].to?.name, "alpha");
+});
