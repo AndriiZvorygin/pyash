1995e6f464b9ab5e27346c861759149942a0753e
Andrii Logan Zvorygin <andrii@liberit.ca>
2026-01-28 14:19:08 -0500

    build then run container

--- container/build.sh
+++ container/build.sh
@@ -5,3 +5,4 @@ ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
 docker compose -f "$ROOT_DIR/container/orchestrate.yaml" down
 docker compose -f "$ROOT_DIR/container/orchestrate.yaml" build
+"$ROOT_DIR/container/run.sh"
