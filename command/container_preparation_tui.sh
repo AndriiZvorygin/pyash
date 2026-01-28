de91b8ab5c02765b2c100ad20f08c6871934a5f7
Andrii Logan Zvorygin <andrii@liberit.ca>
2026-01-27 23:00:54 -0500

    allow disabling dialog mode

--- command/container_preparation_tui.sh
+++ command/container_preparation_tui.sh
@@ -50,7 +50,9 @@ OPENAI_DETECTED=$(get_text openai_detected)
 OPENAI_DETECTED_NONE=$(get_text openai_detected_none)
 
 has_dialog="no"
-if command -v dialog >/dev/null 2>&1; then
+if [[ "${PYA_NO_DIALOG:-}" == "1" ]]; then
+  has_dialog="no"
+elif command -v dialog >/dev/null 2>&1; then
   has_dialog="yes"
 fi
 
