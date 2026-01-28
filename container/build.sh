15ee60d809aaa59d19f58070a67d863d83ea896c
Andrii Logan Zvorygin <andrii@liberit.ca>
2026-01-28 14:16:25 -0500

    restart compose on build

--- container/build.sh
+++ container/build.sh
@@ -3,4 +3,5 @@ set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
+docker compose -f "$ROOT_DIR/container/orchestrate.yaml" down
 docker compose -f "$ROOT_DIR/container/orchestrate.yaml" build
