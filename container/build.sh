adc72cb7822440449a3d02eb2815c09229bf6dcc
Andrii Logan Zvorygin <andrii@liberit.ca>
2026-01-30 17:31:17 -0500

    adjust container service paths and ignores

--- container/build.sh
+++ container/build.sh
@@ -3,6 +3,6 @@ set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
-docker compose -f "$ROOT_DIR/container/service/pyash.yaml" down
 docker compose -f "$ROOT_DIR/container/service/pyash.yaml" build
+docker compose -f "$ROOT_DIR/container/service/pyash.yaml" down
 "$ROOT_DIR/container/command/begin.sh"
