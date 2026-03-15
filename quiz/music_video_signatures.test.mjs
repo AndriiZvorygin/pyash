97cef96c8870b5bcf919cb8f689b944945b06f68
Tatiana Zvorygina <tatiana@example.com>
2026-03-15 08:39:14 -0400

    Revert "add lyric paced music video cuts"
    This reverts commit 2c5929d7121eff4307a2f51e96529c6ee90e6e06.

--- quiz/music_video_signatures.test.mjs
+++ quiz/music_video_signatures.test.mjs
@@ -17,20 +17,4 @@ test("music video footnotes use karaoke mode explicitly", async () => {
     text,
     /from filename of ob of captions output stage with filename of ob of concatenate stage to filename of ob of footnote filename stage by num of ob of subtitle margin ratio stage as text of ob of footnote mode stage be footnote mode do/u
   );
-  assert.match(
-    text,
-    /If current_cut is a transition or instrumental bridge, use previous_cut and next_cut to create an in-between visual inspired by the lyric section that just ended or is about to begin\./u
-  );
-  assert.match(
-    text,
-    /su name music video from filename audio with filename lyrics be ceremony def/u
-  );
-  assert.match(
-    text,
-    /node command\/lyrics_to_srt_from_timing\.mjs/u
-  );
-  assert.match(
-    text,
-    /node command\/srt_to_music_video_itinerary\.mjs/u
-  );
 });
